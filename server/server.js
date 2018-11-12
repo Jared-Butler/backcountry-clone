20c9d0702ad2f91b54dce27ff19552bc24a41f05
Jared Butler <jaredbtlr21@gmail.com>
2018-11-12 12:09:26 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+require('dotenv').config();
+const massive = require('massive');
+
+const {SERVER_PORT, MASSIVE_CONNECTION} = process.env;
+
+const app = express();
+
+massive(MASSIVE_CONNECTION).then(db => {
+    app.set('db',db);
+    console.log('Database Connected');
+})
+
+
+//Middleware
+app.use(express.json());
+
+// How to create sub folders in the db folder and call them in the server using massive.
+// app.get('api/test',(req, res) => {req.get('db').folder.file})
+
+
+
+app.listen(SERVER_PORT, console.log(`The Great Outdoors is calling on port ${SERVER_PORT}`));
