fc25841d6ed70be1a6f3d945a7690ace29efa507
Jared Butler <jaredbtlr21@gmail.com>
2018-11-12 12:11:57 -0700

    basic server and DB connection established.

--- server/server.js
+++ server/server.js
@@ -18,6 +18,6 @@ app.use(express.json());
 // How to create sub folders in the db folder and call them in the server using massive.
 // app.get('api/test',(req, res) => {req.get('db').folder.file})
 
-
+ 
 
 app.listen(SERVER_PORT, console.log(`The Great Outdoors is calling on port ${SERVER_PORT}`));
